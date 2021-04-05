2fd58bc885ed78ee79147c229f4c92434c46d153
Fabian Rico Vargas <Fabian.D.Rico.Vargas@gds.ey.com>
2021-04-05 00:27:48 -0300

    Lecture 83

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,16 @@ import {
     getProductById,
     deleteProduct,
     updateProduct,
-    createProduct
+    createProduct,
+    createProductReview
 } from '../controllers/productController.js'
 import {protect, admin} from '../middleware/authMiddleWare.js'
 
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+
+router.route('/:id/reviews').post(protect, createProductReview)
+
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).
 put(protect, admin, updateProduct)
 
