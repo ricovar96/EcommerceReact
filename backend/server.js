77f795fb3cf143dabde90bd3b476785c0557b0e0
Fabian Rico Vargas <Fabian.D.Rico.Vargas@gds.ey.com>
2021-04-14 16:34:15 -0300

    Node.js was updated2

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
-import { notFound, errorHandler } from './middleWare/errorMiddleware.js'
+import { notFound, errorHandler } from './middleWare/errorMiddleWare.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
