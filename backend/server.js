56d50ee3693313441d4f23e8fbccf0be10d0a479
Fabian Rico Vargas <Fabian.D.Rico.Vargas@gds.ey.com>
2021-04-14 16:41:00 -0300

    Node.js was updated3

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
-import { notFound, errorHandler } from './middleWare/errorMiddleWare.js'
 import connectDB from './config/db.js'
-
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
+import { notFound, errorHandler } from './middleWare/errorMiddleWare.js'
 
 dotenv.config()
 
