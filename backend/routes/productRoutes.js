9309dffec7db106b6ae15afa836c88a9ba95f4ff
Fabian Rico Vargas <Fabian.D.Rico.Vargas@gds.ey.com>
2021-04-14 15:54:18 -0300

    ProductRoutes.js was updated

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,7 @@ import {
   createProductReview,
   getTopProducts,
 } from '../controllers/productController.js'
-import { protect, admin } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleWare/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
